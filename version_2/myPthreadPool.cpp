b5d78f8391bf4b8c98c5c7ca5f50b82c7a943c5c
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-08-26 17:17:40 +0800

    update myPthreadPool about manger pthread

--- version_2/myPthreadPool.cpp
+++ version_2/myPthreadPool.cpp
@@ -1,98 +1,137 @@
-// =====================================================================================
-// 
-//       Filename:  myPthreadPool.cpp
-//
-//    Description:  
-//
-//        Version:  1.0
-//        Created:  2014年05月02日 22时51分28秒
-//       Revision:  none
-//       Compiler:  g++
-//
-//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
-//        Company:  Class 1204 of Computer Science and Technology
-// 
-// =====================================================================================
+/*
+ * =====================================================================================
+ *
+ *       Filename:  myPthreadPool.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年08月08日 11时25分03秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+ *        Company:  Class 1204 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
 
-#include "myPthreadPool.h"
-#include "myPthread.h"
-#include "myCond.h"
-#include "myMutex.h"
+#include <iostream>
 #include <queue>
-#include "myCond.cpp"
-#include "myMutex.cpp"
+#include <vector>
+#include "myPthreadPool.h"
+#include "myWork.h"
+#include "myMutex.h"
+#include "myCond.h"
+#include "myPthread.h"
 
 using std::cout;
+using std::cin;
 using std::endl;
 using std::queue;
+using std::vector;
 
 namespace gaoyuan{
-int MyPthreadPool::current_queue_size = 0;
-pthread_mutex_t MyPthreadPool::mutex;
+pthread_t MyPthreadPool::manage_pthread;
 pthread_cond_t MyPthreadPool::cond;
+pthread_mutex_t MyPthreadPool::mutex;
+pthread_mutex_t MyPthreadPool::pthread_queue_mutex;
+queue <MyWork *> MyPthreadPool::my_work_queue;
+queue <pthread_t *> MyPthreadPool::pthread_work_queue;
+queue <pthread_t *> MyPthreadPool::pthread_relax_queue;
+int MyPthreadPool::max_pthread_num = 0;
+int MyPthreadPool::current_pthread_num = 0;
+int MyPthreadPool::exit_pthread_num = 0;
+int MyPthreadPool::current_work_size = 0;
 bool MyPthreadPool::destory_flag = false;
-int MyPthreadPool::max_thread_num = 0;
-queue <MyWork *>MyPthreadPool::task_queue;
-queue <pthread_t *>MyPthreadPool::pthread_queue;
 
 void MyPthreadPool::my_pthread_pool_init(){
 	MyMutex::mutex_init(&mutex);
+	MyMutex::mutex_init(&pthread_queue_mutex);
 	MyCond::cond_init(&cond);
-	pthread = new pthread_t[this->max_thread_num];
 
-	for (int i = 0 ; i < this->max_thread_num; i++) {
-		MyPthread::my_pthread_create(&pthread[i], my_thread_pool_routine);
-		pthread_queue.push(&pthread[i]);
+	for (int i = 0; i < this->max_pthread_num; i++){
+		pthread_t pthread_tmp;
+
+		MyPthread::my_pthread_create(&pthread_tmp, my_pthread_pool_routine);
+		pthread_work_queue.push(&pthread_tmp);
+		current_pthread_num++;
 	}
 }
+int MyPthreadPool::my_pthread_pool_routine(void *argv){
+	cout << "Pthread " << MyPthread::my_pthread_self_id() << " is starting " << endl;
 
-void *MyPthreadPool::my_thread_pool_routine(void *argv) {
-	cout << "pthread " << MyPthread::my_pthread_self_id()  << "is starting" << endl;
-
-	while (1){
+	while (true){
 		MyMutex::mutex_lock(&mutex);
-		while ((0 == current_queue_size) && (destory_flag == false)) {
-			cout << "the queue of task is null, thread " << MyPthread::my_pthread_self_id() << " is waitting " << endl;
+
+		while ((current_work_size == 0) && (destory_flag == false)){
+			cout << "Pthread " << MyPthread::my_pthread_self_id() << " is sleep to wait for work" << endl;
 			MyCond::cond_wait(&cond, &mutex);
-		} 
-		if (true == destory_flag ) {
+
+			if (exit_pthread_num > 0){
+				exit_pthread_num--;
+				cout << "Pthread " << MyPthread::my_pthread_self_id() << " is exiting " << endl;
+				MyMutex::mutex_unlock(&mutex);
+				MyPthread::my_pthread_exit(NULL);
+			}
+		}
+		if (destory_flag == true){
 			MyMutex::mutex_unlock(&mutex);
-			cout << "phtread  " << MyPthread::my_pthread_self_id() << " will get over " << endl;
+			cout << "Pthread " << MyPthread::my_pthread_self_id << " is exiting " << endl;
 			MyPthread::my_pthread_exit(NULL);
 		}
-		cout << "pthread " << MyPthread::my_pthread_self_id() << " begin to  work now " << endl;
 
-		current_queue_size--;
-		
-		MyWork *tmp_work;
-		tmp_work = (task_queue.front());
-		task_queue.pop();
+		cout << "Pthread " << MyPthread::my_pthread_self_id() << " begin to work now " << endl;
+
+		MyWork *tmp_work = (my_work_queue.front());
+		my_work_queue.pop();
 		MyMutex::mutex_unlock(&mutex);
-		(tmp_work->work)((int *)tmp_work->arg);
-		}
+		(tmp_work)((int *)tmp_work->arg);
+	}
+	MyPthread::my_pthread_exit(NULL);
+	return 0;
 }
-void MyPthreadPool::my_pthread_pool_add_work(void *(*work)(void *), void *arg) {
+int MyPthreadPool::my_pthread_pool_add_work(void *(*work)(void *), void *arg){
 	MyMutex::mutex_lock(&mutex);
-	task_queue.push(&test[*(int *) arg]);
-	current_queue_size++;
-	MyMutex::mutex_unlock(&mutex);
+	my_work_queue.push(work);
+	current_work_size++;
 	MyCond::cond_signal(&cond);
-}
-void MyPthreadPool::destory_pthread_pool() {
-	destory_flag = true;
-	MyCond::cond_broadcast(&cond);
+	MyMutex::mutex_unlock(&mutex);
 
-	for (int i = 0; i < max_thread_num; i++ ) {
-		pthread_t *pthread_tmp;
-		pthread_tmp = pthread_queue.front();
-		pthread_join((*pthread_tmp), NULL);
-		pthread_queue.pop();
+	return 0;
+}
+void MyPthreadPool::my_pthread_manager(void *arg){
+	while (destory_flag == false){
+		sleep(50);
+
+		int work_size = current_work_size;
+		int pthread_num = current_pthread_num;
+
+
+		if (pthread_num < work_size / 3){
+			MyMutex::mutex_lock(&mutex);
+
+			cout << "There are too many works and too less pthreads, we should add some pthreads" << endl;
+			
+			for (int i = pthread_num; i < work_size / 3; i++){
+				pthread_t pthread_tmp;
+
+				MyPthread::my_pthread_create(&pthread_tmp, my_pthread_pool_routine);
+				pthread_work_queue.push(&pthread_tmp);
+				current_pthread_num++;
+			}
+		}
+		if (pthread_num / 3 > work_size ){
+			MyMutex::mutex_lock(&mutex);
+			exit_pthread_num = pthread_num / 3 - work_size;
+			MyMutex::mutex_unlock(&mutex);
+
+			cout << "There are too mant pthreads and too less works, we should kill some pthreads" << endl;
+			for (int i = 0; i < exit_pthread_num; i++){
+				MyCond::cond_signal(&cond);
+			}
+		}
 	}
-	while (task_queue.empty() != true) {
-		task_queue.pop();
-	}
-	MyCond::cond_destroy(&cond);
-	MyMutex::mutex_destory(&mutex);
 }
 }
 
--- version_2/myPthreadPool.h
+++ version_2/myPthreadPool.h
@@ -1,55 +1,60 @@
-// =====================================================================================
-// 
-//       Filename:  myPthreadPool.h
-//
-//    Description:  
-//
-//        Version:  1.0
-//        Created:  2014年05月02日 20时43分18秒
-//       Revision:  none
-//       Compiler:  g++
-//
-//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
-//        Company:  Class 1204 of Computer Science and Technology
-// 
-// =====================================================================================
-
-#ifndef _MYPTHREADPOOL_H
-#define _MYPTHREADPOOL_H
+/*
+ * =====================================================================================
+ *
+ *       Filename:  myPthreadPool.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年08月07日 09时59分11秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+ *        Company:  Class 1204 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+#ifndef _MY_PTHREADPOOL_H
+#define _MY_PTHREADPOOL_H
 
 #include <iostream>
-#include <pthread.h>
 #include <queue>
-#include "myWork.h"
+#include <vector>
+#include <pthread.h>
+#include "myWord.h"
 
 using std::queue;
 
 namespace gaoyuan{
-  
- int i [10] = {1,2,3,4,5,6,7,8,9,10};
-MyWork test[10] = {MyWork(work, (void *)&i[0]), MyWork(work, (void *)&i[2]), MyWork(work, (void *)&i[3]), MyWork(work, (void *)&i[4]), MyWork(work, (void *)&i[5]), MyWork(work, (void *)&i[6]), MyWork(work, (void *)&i[7]), MyWork(work, (void *)&i[8]), MyWork(work, (void *)&i[9]), MyWork(work, (void *) &i[2]) };
+	class MyPthread my_pthread;
 
-
-  class MyPthreadPool {
-	public:
-		MyPthreadPool(int max_thread_num){this->max_thread_num = max_thread_num;}
-		void my_pthread_pool_init();
-		static void *my_thread_pool_routine(void *arg);
-		void my_pthread_pool_add_work(void* (*work)(void *arg), void * arg);
-		void destory_pthread_pool();
-	private:
-		    pthread_t *pthread;
-static		pthread_cond_t cond;
-static		pthread_mutex_t mutex;
-static		queue <MyWork *> task_queue;
-static		queue <pthread_t *> pthread_queue;
-static      int max_thread_num;
-static      int current_queue_size;
-static		bool destory_flag;
-};
-
+	class MyPthreadPool{
+		public:
+			MyPthreadPool(int max_pthread_num = 0){
+				this->max_pthread_num = max_pthread_num;
+			}
+			void my_pthread_pool_init();
+			static int my_pthread_pool_routine(void *arg);
+			static void my_pthread_manager(void *arg);
+			static bool if_pthread_alive(pthread_t pid);
+			void my_pthread_pool_add_work(void *(* work)(void *arg), void * arg);
+			void my_pthread_pool_destory();
+		private:
+			static pthread_t manage_pthread;
+			static pthread_cond_t cond;
+			static pthread_mutex_t mutex;
+			static pthread_mutex_t pthread_queue_mutex;
+			static queue<MyWork *> my_work_queue;
+			static queue<pthread_t *> pthread_work_queue;
+			static queue<pthread_t *> pthread_relax_queue;
+			static int max_pthread_num;
+			static int current_pthread_num;
+			static int exit_pthread_num;
+			static int current_work_size;
+			static bool destory_flag;
+	}
 }
+
 #endif
-
-
 
